707cf72e6220b13d139d5874c23632a27eb1fb64
蔡宝林 <401115406@qq.com>
2022-05-25 16:04:06 +0800

    媒体数据获取逻辑添加

--- base/src/main/java/com/cbl/base/BaseApp.kt
+++ base/src/main/java/com/cbl/base/BaseApp.kt
@@ -2,25 +2,66 @@ package com.cbl.base
 
 import android.app.Application
 import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
+import android.provider.MediaStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 open class BaseApp : Application() {
-    companion object{
+    val refresh = MutableStateFlow(SystemClock.uptimeMillis())
+    val mainHandler = Handler(Looper.getMainLooper())
+
+    companion object {
         lateinit var CONTEXT: Context
-        val scope= CoroutineScope(SupervisorJob() + Dispatchers.Main)
+        val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
 
     }
+
     override fun onCreate() {
         super.onCreate()
-        CONTEXT=this
+        CONTEXT = this
         Timber.plant(Timber.DebugTree())
         Timber.i("onCreate")
+
+
+        val contentObserver = object : ContentObserver(mainHandler) {
+            override fun onChange(selfChange: Boolean) {
+                super.onChange(selfChange)
+                val time = SystemClock.uptimeMillis();
+                Timber.i("onChange time-->$time Thread--> ${Thread.currentThread().name}")
+                scope.launch {
+                    refresh.emit(time)
+                    Timber.i("onChange emit over time-->$time")
+                }
+            }
+
+        }
+        contentResolver.registerContentObserver(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            true,
+            contentObserver
+        )
+        contentResolver.registerContentObserver(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            true,
+            contentObserver
+        )
         scope.launch(Dispatchers.IO) {
-            MediaUtil.getData()
+            refresh.collectLatest {
+                Timber.i("collectLatest it-->$it  Thread--> ${Thread.currentThread().name}")
+                MediaUtil.getData()
+                Timber.i("collectLatest it over-->$it")
+
+            }
         }
     }
 }
--- base/src/main/java/com/cbl/base/MediaUtil.kt
+++ base/src/main/java/com/cbl/base/MediaUtil.kt
@@ -3,10 +3,14 @@ package com.cbl.base
 import android.annotation.SuppressLint
 import android.database.Cursor
 import android.provider.MediaStore
+import kotlinx.coroutines.*
 import timber.log.Timber
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 object MediaUtil {
+    val start = AtomicBoolean(false)
+
     /**
      * 需要从数据库中获取的信息：
      * BUCKET_DISPLAY_NAME  文件夹名称
@@ -18,25 +22,34 @@ object MediaUtil {
     )
 
     @SuppressLint("Range")
-    fun getData() {
+    suspend fun getData() {
+        start.set(true)
+        Timber.i("getData start thread-->${Thread.currentThread().name}")
         /**
          * 通过ContentResolver 从媒体数据库中读取图片信息
          */
         var cursor: Cursor? = BaseApp.CONTEXT.getContentResolver().query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,  //限制类型为图片
             projection,
-            MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?",
-            arrayOf("image/jpeg", "image/png"),  // 这里筛选了jpg和png格式的图片
+            null,
+            null,  // 这里筛选了jpg和png格式的图片
             MediaStore.Images.Media.DATE_ADDED
         ) // 排序方式：按添加时间排序
+        Timber.i("getData  thread-->${Thread.currentThread().name}  count-->${cursor?.count}")
+        var i = 0
         cursor?.let {
             while (cursor.moveToNext()) {
                 val displayName =
                     cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME))
-                val path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
-                Timber.i("displayName-->$displayName path-->$path")
+                val path =
+                    cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+//                        Timber.i("displayName-->$displayName path-->$path")
+                i++
             }
         }
+        Timber.i("getData over thread-->${Thread.currentThread().name} i-->$i")
+        start.set(false)
+
     }
 
 
