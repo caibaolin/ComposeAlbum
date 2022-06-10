97e076f7c685a11b041c2bf3083d0aacc5aa4008
蔡宝林 <401115406@qq.com>
2022-06-10 18:33:58 +0800

    完善coil调用

--- app/src/main/java/com/cbl/composealbum/view/View.kt
+++ app/src/main/java/com/cbl/composealbum/view/View.kt
@@ -1,6 +1,8 @@
 package com.cbl.base.view
 
 import android.R
+import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,8 +14,16 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.VideoFrameDecoder
 import coil.request.ImageRequest
+import coil.request.videoFrameMicros
+import coil.request.videoFrameMillis
+import coil.transition.Transition
 import com.cbl.base.bean.AlbumBean
 import com.cbl.base.bean.MediaBean
 
@@ -42,20 +52,49 @@ fun LeftLazyItem(albumBean: AlbumBean, onClick: () -> Unit, clickSelect: Boolean
 
 @Composable
 fun RightLazyItem(mediaBean: MediaBean, onClick: () -> Unit) {
-    AsyncImage(
+   /* val imageLoader = ImageLoader.Builder(LocalContext.current)
+        .components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()*/
+/*    AsyncImage(
         model = mediaBean.path,
         contentDescription = null,
         modifier = Modifier.aspectRatio(1f),
         contentScale = ContentScale.Crop
-    )
-  /*  AsyncImage(
+    )*/
+/*    Image(
+        modifier = Modifier
+            .size(300.dp),
+        painter = rememberAsyncImagePainter(
+            model = "https://img-blog.csdnimg.cn/e6bb95f89399404195212196fdaf3964.gif",
+            builder = {
+                decoder(GifDecoder())//添加GIF解码
+                placeholder(R.drawable.head_icon)//占位图
+                crossfade(true)//淡出效果
+            }),
+        contentDescription = null
+    )*/
+    AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
-            .data("https://example.com/image.jpg")
+            .data(mediaBean.path)
+            .videoFrameMillis(1L)
             .crossfade(true)
+            .apply {
+                if (mediaBean.mimeType?.contains("video") == true){
+                    decoderFactory(VideoFrameDecoder.Factory())
+                }else if(mediaBean.mimeType?.contains("gif") == true){
+                    decoderFactory(GifDecoder.Factory())
+                }
+            }
             .build(),
-        placeholder = painterResource(R.drawable.placeholder),
+//        placeholder = painterResource(R.drawable.placeholder),
         contentDescription=null,
         contentScale = ContentScale.Crop,
         modifier = Modifier.aspectRatio(1f),
-    )*/
+    )
 }
